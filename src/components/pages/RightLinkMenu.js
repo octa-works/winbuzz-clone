55e77fa52496c9e1d4cb316ebad89056a5f638c8
octa-works <alwaysrocks451256@gmail.com>
2025-01-14 12:12:05 +0530

    fixed right side content

--- src/components/pages/RightLinkMenu.js
+++ src/components/pages/RightLinkMenu.js
@@ -5,6 +5,15 @@ import dragontiger from "../../assests/images/rightBanner/dragontiger.webp";
 import lucky7 from "../../assests/images/rightBanner/lucky7.webp";
 import andar from "../../assests/images/rightBanner/andar-bahar.webp";
 
+import casino4 from "../../assests/images/secondRightBanner/casino4.webp";
+import casino5 from "../../assests/images/secondRightBanner/casino5.webp";
+import casino6 from "../../assests/images/secondRightBanner/casino6.webp";
+import casino8 from "../../assests/images/secondRightBanner/casino9.webp";
+import casino9 from "../../assests/images/secondRightBanner/casino8.webp";
+import casino10 from "../../assests/images/secondRightBanner/casino10.webp";
+import casino12 from "../../assests/images/secondRightBanner/casino12.webp";
+import dreamcatch from "../../assests/images/secondRightBanner/casino-dreamcatcher.webp";
+
 function RightLinkMenu() {
   return (
     <div className="border right-link-menu mt-3">
@@ -26,34 +35,34 @@ export function RightSecondLinkMenu() {
       <div className="p-1">Play Games</div>
       <div class="row mt-1">
         <div class="col">
-          <img src={routlett} alt="banner" className="img-fluid" />
+          <img src={casino4} alt="banner" className="img-fluid" />
         </div>
         <div class="col">
-          <img src={routlett} alt="banner" className="img-fluid" />
-        </div>
-      </div>
-      <div class="row">
-        <div class="col">
-          <img src={routlett} alt="banner" className="img-fluid" />
-        </div>
-        <div class="col">
-          <img src={routlett} alt="banner" className="img-fluid" />
+          <img src={casino5} alt="banner" className="img-fluid" />
         </div>
       </div>
       <div class="row">
         <div class="col">
-          <img src={routlett} alt="banner" className="img-fluid" />
+          <img src={casino6} alt="banner" className="img-fluid" />
         </div>
         <div class="col">
-          <img src={routlett} alt="banner" className="img-fluid" />
+          <img src={casino8} alt="banner" className="img-fluid" />
         </div>
       </div>
       <div class="row">
         <div class="col">
-          <img src={routlett} alt="banner" className="img-fluid" />
+          <img src={casino9} alt="banner" className="img-fluid" />
         </div>
         <div class="col">
-          <img src={routlett} alt="banner" className="img-fluid" />
+          <img src={casino10} alt="banner" className="img-fluid" />
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <img src={dreamcatch} alt="banner" className="img-fluid" />
+        </div>
+        <div class="col">
+          <img src={casino12} alt="banner" className="img-fluid" />
         </div>
       </div>
     </div>
