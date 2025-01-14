c2d902d750d4f22ff39f12e16e5dc1683994a1d2
octa-works <alwaysrocks451256@gmail.com>
2025-01-14 12:32:50 +0530

    fixed right side

--- src/components/pages/RightLinkMenu.js
+++ src/components/pages/RightLinkMenu.js
@@ -33,36 +33,38 @@ export function RightSecondLinkMenu() {
   return (
     <div className=" border right-link-menu mt-3 ">
       <div className="p-1">Play Games</div>
-      <div class="row mt-1">
-        <div class="col">
-          <img src={casino4} alt="banner" className="img-fluid" />
+      <div className=" column-gap-0 ">
+        <div class="row mt-1">
+          <div class="col">
+            <img src={casino4} alt="banner" className="img-fluid" />
+          </div>
+          <div class="col">
+            <img src={casino5} alt="banner" className="img-fluid" />
+          </div>
         </div>
-        <div class="col">
-          <img src={casino5} alt="banner" className="img-fluid" />
+        <div class="row">
+          <div class="col">
+            <img src={casino6} alt="banner" className="img-fluid" />
+          </div>
+          <div class="col">
+            <img src={casino8} alt="banner" className="img-fluid" />
+          </div>
         </div>
-      </div>
-      <div class="row">
-        <div class="col">
-          <img src={casino6} alt="banner" className="img-fluid" />
+        <div class="row">
+          <div class="col">
+            <img src={casino9} alt="banner" className="img-fluid" />
+          </div>
+          <div class="col">
+            <img src={casino10} alt="banner" className="img-fluid" />
+          </div>
         </div>
-        <div class="col">
-          <img src={casino8} alt="banner" className="img-fluid" />
-        </div>
-      </div>
-      <div class="row">
-        <div class="col">
-          <img src={casino9} alt="banner" className="img-fluid" />
-        </div>
-        <div class="col">
-          <img src={casino10} alt="banner" className="img-fluid" />
-        </div>
-      </div>
-      <div class="row">
-        <div class="col">
-          <img src={dreamcatch} alt="banner" className="img-fluid" />
-        </div>
-        <div class="col">
-          <img src={casino12} alt="banner" className="img-fluid" />
+        <div class="row">
+          <div class="col">
+            <img src={dreamcatch} alt="banner" className="img-fluid" />
+          </div>
+          <div class="col">
+            <img src={casino12} alt="banner" className="img-fluid" />
+          </div>
         </div>
       </div>
     </div>
