e2401c0e08b798ea285ec233675fce90df21bbb0
octa-works <alwaysrocks451256@gmail.com>
2025-01-14 15:09:15 +0530

    add icons

--- src/assests/icons/IconsPack.js
+++ src/assests/icons/IconsPack.js
@@ -1,6 +1,30 @@
 import React from "react";
 import { AiFillAndroid } from "react-icons/ai";
+import { MdSportsCricket } from "react-icons/md";
+import { IoFootball } from "react-icons/io5";
+import { BiSolidTennisBall } from "react-icons/bi";
+import { GiPublicSpeaker } from "react-icons/gi";
+import { GiCardAceHearts } from "react-icons/gi";
+import { FcStatistics } from "react-icons/fc";
+import { GiTrophy } from "react-icons/gi";
+import { FaHorse } from "react-icons/fa6";
+import { GiHound } from "react-icons/gi";
+import { ImStatsBars } from "react-icons/im";
+import { MdOutlineSportsKabaddi } from "react-icons/md";
+import { IoIosBasketball } from "react-icons/io";
 
-export default IconsPack = {
+export const IconsPack = {
   Android: AiFillAndroid,
+  Cricket: MdSportsCricket,
+  Football: IoFootball,
+  Tennis: BiSolidTennisBall,
+  Politics: GiPublicSpeaker,
+  Casino: GiCardAceHearts,
+  INTCasino: FcStatistics,
+  SportsBook: GiTrophy,
+  HorseRacing: FaHorse,
+  GreyHound: GiHound,
+  Binary: ImStatsBars,
+  Kabaddi: MdOutlineSportsKabaddi,
+  Basketball: IoIosBasketball,
 };
--- src/components/AppSlideNav.js
+++ src/components/AppSlideNav.js
@@ -1,8 +1,135 @@
 import React from "react";
-import NavbarList, { NavList } from "./NavbarList";
+import { NavList } from "./NavbarList";
+import { IconsPack } from "../assests/icons/IconsPack";
 
 function AppSlideNav() {
-  return <div>{NavList.Nav1}</div>;
+  return (
+    <div>
+      <div className="list-group">
+        <a
+          href="#"
+          className="list-group-item list-group-item-action list-group-item-warning"
+          aria-current="true"
+        >
+          <IconsPack.Cricket /> {NavList.Nav1}
+        </a>
+        <a
+          href="#"
+          className="list-group-item list-group-item-action list-group-item-warning"
+        >
+          {NavList.Nav2}
+        </a>
+        <a
+          href="#"
+          className="list-group-item list-group-item-action list-group-item-warning"
+        >
+          {NavList.Nav3}
+        </a>
+        <a
+          href="#"
+          className="list-group-item list-group-item-action list-group-item-warning"
+        >
+          {NavList.Nav4}
+        </a>
+        <a
+          href="#"
+          className="list-group-item list-group-item-action list-group-item-warning"
+        >
+          {NavList.Nav5}
+        </a>
+        <a
+          href="#"
+          className="list-group-item list-group-item-action list-group-item-warning"
+        >
+          {NavList.Nav6}
+        </a>
+        <a
+          href="#"
+          className="list-group-item list-group-item-action list-group-item-warning"
+        >
+          {NavList.Nav7}
+        </a>
+        <a
+          href="#"
+          className="list-group-item list-group-item-action list-group-item-warning"
+        >
+          {NavList.Nav8}
+        </a>
+        <a
+          href="#"
+          className="list-group-item list-group-item-action list-group-item-warning"
+        >
+          {NavList.Nav9}
+        </a>
+        <a
+          href="#"
+          className="list-group-item list-group-item-action list-group-item-warning"
+        >
+          {NavList.Nav10}
+        </a>
+        <a
+          href="#"
+          className="list-group-item list-group-item-action list-group-item-warning"
+        >
+          {NavList.Nav11}
+        </a>
+        <a
+          href="#"
+          className="list-group-item list-group-item-action list-group-item-warning"
+        >
+          {NavList.Nav12}
+        </a>
+        <a
+          href="#"
+          className="list-group-item list-group-item-action list-group-item-warning"
+        >
+          {NavList.Nav13}
+        </a>
+        <a
+          href="#"
+          className="list-group-item list-group-item-action list-group-item-warning"
+        >
+          {NavList.Nav14}
+        </a>
+        <a
+          href="#"
+          className="list-group-item list-group-item-action list-group-item-warning"
+        >
+          {NavList.Nav15}
+        </a>
+        <a
+          href="#"
+          className="list-group-item list-group-item-action list-group-item-warning"
+        >
+          {NavList.Nav16}
+        </a>
+        <a
+          href="#"
+          className="list-group-item list-group-item-action list-group-item-warning"
+        >
+          {NavList.Nav17}
+        </a>
+        <a
+          href="#"
+          className="list-group-item list-group-item-action list-group-item-warning"
+        >
+          {NavList.Nav18}
+        </a>
+        <a
+          href="#"
+          className="list-group-item list-group-item-action list-group-item-warning"
+        >
+          {NavList.Nav19}
+        </a>
+        <a
+          href="#"
+          className="list-group-item list-group-item-action list-group-item-warning"
+        >
+          {NavList.Nav20}
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default AppSlideNav;
