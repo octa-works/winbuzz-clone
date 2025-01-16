5e2d7bf56cefdfaabfc47989c99ebe062d47dc79
octa-works <alwaysrocks451256@gmail.com>
2025-01-16 12:25:14 +0530

    add file css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
 import "./App.css";
+import "../src/assests/stylesheet/Mobile/mobile.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 
