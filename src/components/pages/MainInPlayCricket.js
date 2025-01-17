0c5287d88d1e312ef9fc27b7292bfa76b68c148d
octa-works <alwaysrocks451256@gmail.com>
2025-01-17 10:53:46 +0530

    add table

--- src/components/pages/MainInPlayCricket.js
+++ src/components/pages/MainInPlayCricket.js
@@ -5,34 +5,37 @@ function MainInPlayCricket() {
     <div>
       <div>INPLAY</div>
       <div>
-        <table class="table-primary">Table</table>
-        <table class="table-secondary">Table 1</table>
-        <table class="table-success">...</table>
-        <table class="table-danger">...</table>
-        <table class="table-warning">...</table>
-        <table class="table-info">...</table>
-        <table class="table-light">...</table>
-        <table class="table-dark">...</table>
-
-        <tr class="table-primary">...</tr>
-        <tr class="table-secondary">...</tr>
-        <tr class="table-success">...</tr>
-        <tr class="table-danger">...</tr>
-        <tr class="table-warning">...</tr>
-        <tr class="table-info">...</tr>
-        <tr class="table-light">...</tr>
-        <tr class="table-dark">...</tr>
-
-        <tr>
-          <td class="table-primary">...</td>
-          <td class="table-secondary">...</td>
-          <td class="table-success">...</td>
-          <td class="table-danger">...</td>
-          <td class="table-warning">...</td>
-          <td class="table-info">...</td>
-          <td class="table-light">...</td>
-          <td class="table-dark">...</td>
-        </tr>
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">
+                <div>Twenty20 Big Bash</div>
+              </th>
+              <th scope="col">
+                <button>BM</button>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">Australia Women vs England Women</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td colspan="2">Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
