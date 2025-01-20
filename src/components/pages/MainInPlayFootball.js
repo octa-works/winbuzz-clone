81a3227f89d5d5fddc40f73153812915c13e77b6
octa-works <alwaysrocks451256@gmail.com>
2025-01-20 11:37:42 +0530

    add inplay football

--- src/components/pages/MainInPlayFootball.js
+++ src/components/pages/MainInPlayFootball.js
@@ -1,7 +1,261 @@
 import React from "react";
+import BlinkDots from "../../assests/icons/BlinkDots";
+import "../../assests/stylesheet/Desktop/TableDesign.css";
+import { IconsPack } from "../../assests/icons/IconsPack";
+
+const currentDate = new Date().toLocaleDateString();
+const currentTime = new Date().toLocaleTimeString();
 
 function MainInPlayFootball() {
-  return <div>MainInPlayFootball</div>;
+  return (
+    <div>
+      <div>
+        <div className="float-start pt-2 pb-2 mt-2 mb-2">
+          <IconsPack.Inplay className="bg-warning rounded" /> INPLAY
+        </div>
+        <div>
+          <table class="table table-hover border border-1 rounded-3 table-responsive">
+            <thead className="table-success rounded">
+              <tr className="table-style">
+                <th scope="col">
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div className="tb-text">
+                      <IconsPack.Cricket2 className="me-2" />
+                      CRICKET
+                    </div>
+                    <div className="d-flex gap-2">
+                      <button className="tbl-btn border border-danger text-black">
+                        <IconsPack.Plus />
+                        Live
+                      </button>
+                      <button className="tbl-btn border border-danger text-black">
+                        <IconsPack.Plus />
+                        Virtual
+                      </button>
+                    </div>
+                  </div>
+                </th>
+
+                <th scope="col">
+                  <div className="inplayCrickRight d-flex justify-content-between align-items-center ms-2 mb-3">
+                    <div className="ps-3 pe-3">1</div>
+                    <div className="ps-3 pe-3">x</div>
+                    <div className="ps-3 pe-3">2</div>
+                  </div>
+                </th>
+              </tr>
+            </thead>
+            {/*  */}
+            <tbody>
+              {/* First football list */}
+              <tr>
+                <th scope="col">
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div className="tb-text">
+                      <div className="small">
+                        {currentDate}-{currentTime}
+                      </div>
+                      <div>Como V Udinese</div>
+                    </div>
+                    <div className="d-flex">
+                      <BlinkDots />
+                      <button className="tbl-btn">BM</button>
+                    </div>
+                  </div>
+                </th>
+
+                <th scope="col">
+                  <div className="inplayCrickRight d-flex justify-content-between align-items-center ms-2 mb-3">
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                  </div>
+                </th>
+              </tr>
+
+              {/* Second Football list */}
+              <tr>
+                <th scope="col">
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div className="tb-text">
+                      <div className="small">
+                        {currentDate}-{currentTime}
+                      </div>
+                      <div>Como V Udinese</div>
+                    </div>
+                    <div className="d-flex">
+                      <BlinkDots />
+                      <button className="tbl-btn">BM</button>
+                    </div>
+                  </div>
+                </th>
+
+                <th scope="col">
+                  <div className="inplayCrickRight d-flex justify-content-between align-items-center ms-2 mb-3">
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                  </div>
+                </th>
+              </tr>
+
+              {/* Third football */}
+              <tr>
+                <th scope="col">
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div className="tb-text">
+                      <div className="small">
+                        {currentDate}-{currentTime}
+                      </div>
+                      <div>Como V Udinese</div>
+                    </div>
+                    <div className="d-flex">
+                      <BlinkDots />
+                      <button className="tbl-btn">BM</button>
+                    </div>
+                  </div>
+                </th>
+
+                <th scope="col">
+                  <div className="inplayCrickRight d-flex justify-content-between align-items-center ms-2 mb-3">
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                  </div>
+                </th>
+              </tr>
+
+              {/* Fourth */}
+
+              <tr>
+                <th scope="col">
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div className="tb-text">
+                      <div className="small">
+                        {currentDate}-{currentTime}
+                      </div>
+                      <div>Como V Udinese</div>
+                    </div>
+                    <div className="d-flex">
+                      <BlinkDots />
+                      <button className="tbl-btn">BM</button>
+                    </div>
+                  </div>
+                </th>
+
+                <th scope="col">
+                  <div className="inplayCrickRight d-flex justify-content-between align-items-center ms-2 mb-3">
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                  </div>
+                </th>
+              </tr>
+
+              {/* Fifth */}
+              <tr>
+                <th scope="col">
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div className="tb-text">
+                      <div className="small">
+                        {currentDate}-{currentTime}
+                      </div>
+                      <div>Como V Udinese</div>
+                    </div>
+                    <div className="d-flex">
+                      <BlinkDots />
+                      <button className="tbl-btn">BM</button>
+                    </div>
+                  </div>
+                </th>
+
+                <th scope="col">
+                  <div className="inplayCrickRight d-flex justify-content-between align-items-center ms-2 mb-3">
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                  </div>
+                </th>
+              </tr>
+
+              {/* Sixth */}
+              <tr>
+                <th scope="col">
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div className="tb-text">
+                      <div className="small">
+                        {currentDate}-{currentTime}
+                      </div>
+                      <div>Como V Udinese</div>
+                    </div>
+                    <div className="d-flex">
+                      <BlinkDots />
+                      <button className="tbl-btn">BM</button>
+                    </div>
+                  </div>
+                </th>
+
+                <th scope="col">
+                  <div className="inplayCrickRight d-flex justify-content-between align-items-center ms-2 mb-3">
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                  </div>
+                </th>
+              </tr>
+
+              {/* Seventh */}
+              <tr>
+                <th scope="col">
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div className="tb-text">
+                      <div className="small">
+                        {currentDate}-{currentTime}
+                      </div>
+                      <div>Como V Udinese</div>
+                    </div>
+                    <div className="d-flex">
+                      <BlinkDots />
+                      <button className="tbl-btn">BM</button>
+                    </div>
+                  </div>
+                </th>
+
+                <th scope="col">
+                  <div className="inplayCrickRight d-flex justify-content-between align-items-center ms-2 mb-3">
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                    <button className="ps-3 pe-3">-</button>
+                  </div>
+                </th>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MainInPlayFootball;
