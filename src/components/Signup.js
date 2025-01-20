61a60c9d91edb3143583bc9e0a139cb85ce8e2b0
octa-works <alwaysrocks451256@gmail.com>
2025-01-20 15:45:20 +0530

    improving ui

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,10 +1,12 @@
 import React from "react";
+import logo from "../assests/images/winbuzz-logo.png";
 
 function Signup() {
   return (
-    <div className="SignupHeader" style={HeaderDesign}>
+    <div style={HeaderDesign}>
       <div>Div</div>
-      <div>
+      <div style={HeaderDesign.HeaderBorder} className="container">
+        <img src={logo} alt="signup-logo" />
         <div></div>
       </div>
     </div>
@@ -12,10 +14,15 @@ function Signup() {
 }
 
 const HeaderDesign = {
-  backgroundColor: "black",
+  backgroundColor: "#B88831",
   color: "white",
   padding: "10px",
   textAlign: "center",
+  HeaderBorder: {
+    border: "1px solid black",
+    padding: "10px",
+    margin: "10px",
+  },
 };
 
 export default Signup;
