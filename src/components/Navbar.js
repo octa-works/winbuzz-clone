744e68a446a3b701f9f2db581e3789a30b514628
octa-works <alwaysrocks451256@gmail.com>
2025-01-24 13:04:14 +0530

    fix nav responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,15 +8,15 @@ function Navbar() {
   return (
     <div className="bg-success">
       <nav className="navbar bg-dark pt-4">
-        <div className="container-fluid bg-nav p-3 ">
+        <div className="container-fluid nav-con-mob bg-nav p-3">
           <Link to="/winbuzz-clone" className="text-white">
             <img src={logo} alt="logo-winbuzz" height={40} />
           </Link>
           <div className="d-flex nav-search ">
-            <button className=" nav-btn">Rules</button>
+            <button className="nav-btn">Rules</button>
             <form className="d-flex" role="search">
               <input
-                className=" form-control me-2"
+                className=" form-control"
                 type="search"
                 placeholder="Search"
                 aria-label="Search"
@@ -31,14 +31,10 @@ function Navbar() {
               Login
             </button>
             <Login />
-            {/* <button className=" nav-btn " type="button">
-              Register
-            </button> */}
             <Link
               className="nav-btn text-black align-content-center"
               to="/winbuzz-clone/signup"
             >
-              {" "}
               Register
             </Link>
           </div>
